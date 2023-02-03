da57d2dd4aa6be43aa55250ff6babe77e7645b1f
frayfray248 <nicman248@hotmail.com>
2023-02-02 20:55:15 -0800

    User model enforce unique usernames

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        unique: true
+    },
     password: String,
     role: String,
     orders: Object
